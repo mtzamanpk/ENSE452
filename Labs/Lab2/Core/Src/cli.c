27611b415ee1d10f21d7939186c5f4df864cbf21
Muhammad Zaman <mtzamanpk@gmail.com>
2024-09-25 10:05:54 -0600

    phase 1 done

---
+++ Labs/Lab2/Core/Src/cli.c
@@ -0,0 +1,10 @@
+/*
+ * cli.c
+ *
+ *  Created on: Sep 25, 2024
+ *      Author: mtzam
+ */
+
+
+
+
---
+++ Labs/Lab2/Core/Src/cli.h
@@ -0,0 +1,16 @@
+/*
+ * cli.h
+ *
+ *  Created on: Sep 25, 2024
+ *      Author: mtzam
+ */
+
+#ifndef SRC_CLI_H_
+#define SRC_CLI_H_
+
+#ifndef CLI_H
+#define CLI_H
+
+
+
+#endif /* SRC_CLI_H_ */
